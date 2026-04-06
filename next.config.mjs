5242e4570cf92f1f1b45077ad55eac6b42727983
Dave Sousa <appropriate.moose@gmail.com>
2026-04-06 12:35:41 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,33 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com"
-      }
-    ]
-  }
+        hostname: "images.unsplash.com",
+      },
+    ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: "/_next/static/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store, max-age=0, must-revalidate",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
